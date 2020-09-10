6eaff1148f7ec2eb23b8436beb1d2889f6773072
Severin Langberg <langberg91@gmail.com>
2020-09-10 10:10:19 +0200

    Added plotting functions.

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import numpy as np 
+import pandas as pd 
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
-#from utils import load_data
+from utils import plot_confusion_matrix, plot_validation_scores, load_data
 from train import select_hyperparameters, evaluate_model
 
 
 def main():
 
-	X, y = load_data("path_to_data")
-
+	#X, y = load_data("path_to_data")
+	from sklearn.datasets import load_breast_cancer
+	X, y = load_breast_cancer(return_X_y=True)
+	
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 	scaler = StandardScaler()
@@ -19,14 +22,22 @@ def main():
 	X_test_std = scaler.transform(X_test)
 
 	# Define hyperparameter grid.
-	hparam_grid = {"C": 10 ** np.linspace(-2, 2, 10)}
+	hparam_grid = {"C": 10 ** np.linspace(-3, 1, 10)}
 
 	# Initialise model.
 	model = LogisticRegression(random_state=42)
 
-	opt_model = select_hyperparameters(model, hparam_grid, X_train_std, y_train, "path_to_cvresults.csv")
-
+	opt_model = select_hyperparameters(model, hparam_grid, X_train_std, y_train, "cvresults.csv")
+	
+	cv_results = pd.read_csv("cvresults.csv", index_col=0)
+	plot_validation_scores(cv_results, "validation.pdf")
+	
 	evaluate_model(opt_model, X_test_std, y_test, "best_model")
+
+	y_test = np.load("best_model_true.npy")
+	y_pred = np.load("best_model_pred.npy")
+
+	plot_confusion_matrix(y_test, y_pred, "confusion.pdf")
 
 
 if __name__ == "__main__":
